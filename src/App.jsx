e49511911359688e3643d040cbd056478b69dc73
HassanAbbaccy <hassanabbaccy@gmail.com>
2024-08-23 00:30:58 +0500

    navbar sidebar footer and form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 
 import './App.css'
+import SideBar from './components/SideBar'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import CreatePost from './components/CreatePost'
 
 function App() {
   
 
   return (
-    <>
-     <h1></h1>
-    </>
+    <div className='app-container'>
+    <SideBar></SideBar>
+    <div className='content'>
+    <Navbar></Navbar>
+    <CreatePost></CreatePost>
+    <Footer></Footer>
+    </div>
+    </div>
   )
 }
 
