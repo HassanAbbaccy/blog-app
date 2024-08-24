af91782e1704187878d05dce2356c8e2e0deaa09
HassanAbbaccy <hassanabbaccy@gmail.com>
2024-08-24 16:14:41 +0500

    adding usestate and handling events

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import SideBar from './components/SideBar'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import CreatePost from './components/CreatePost'
+import PostList from './components/PostList'
+import { useState } from 'react'
 
 function App() {
-  
+  const [home, setHome]= useState("Create Post");
 
   return (
     <div className='app-container'>
-    <SideBar></SideBar>
+    <SideBar home={home} setHome={setHome}></SideBar>
     <div className='content'>
     <Navbar></Navbar>
-    <CreatePost></CreatePost>
+    {home === "Home" ? <PostList></PostList>:  <CreatePost></CreatePost>}
     <Footer></Footer>
     </div>
     </div>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
 
-const SideBar = () => {
+const SideBar = ({ home, setHome }) => {
   return (
     <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar" style={{width: "180px"}}>
     <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <svg className="bi pe-none me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
       <span className="fs-4">Blog App</span>
     </a>
     <hr/>
     <ul className="nav nav-pills flex-column mb-auto">
       <li className="nav-item">
-        <a href="#" className="nav-link active" aria-current="page">
-          <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="#home"></use></svg>
+        <a href="#" className={`nav-link text-white ${home === 'Home' && 'active'}`} aria-current="page" onClick={() =>{setHome('Home')}}>
           Home
         </a>
       </li>
       <li className="nav-item">
-        <a href="#" className="nav-link text-white" aria-current="page">
-          <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="#home"></use></svg>
+        <a href="#" className={`nav-link text-white ${home === 'Create Post' && 'active'}`} aria-current="page" onClick={() =>{setHome('Create Post')}}>
           Create Post
         </a>
       </li>
